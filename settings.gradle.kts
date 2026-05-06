f2650e59d46fd21c3f6e28b5d972b8dfc68fe59a
sada <295354859@qq.com>
2026-05-06 22:41:28 +0800

    add receive client debug guide

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,9 @@ pluginManagement {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
